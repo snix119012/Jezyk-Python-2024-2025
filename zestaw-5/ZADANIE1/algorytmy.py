1cb689f85674f109e9630371772ef7bc641165fa
snix119012 <147590011+snix119012@users.noreply.github.com>
2024-12-21 19:34:24 +0100

    Add files via upload

---
+++ zestaw-5/ZADANIE1/algorytmy.py
@@ -0,0 +1,145 @@
+from ZADANIE1.tablica import MonitorowanaTablica
+
+def insertion_sort(array: MonitorowanaTablica, left=0, right=None):
+    if right is None:
+        right = len(array) - 1
+
+    i = left + 1
+    while i <= right:
+        j = i
+        while j > left and array[j - 1] > array[j]:
+            array[j - 1], array[j] = array[j], array[j - 1]
+            j -= 1
+        i += 1
+
+
+def bubble_sort(array: MonitorowanaTablica):
+    n = len(array)
+
+    for i in range(n):
+        swapped = False
+
+        for j in range(0, n - i - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+
+                swapped = True
+
+        if not swapped:
+            break
+
+
+def shell_sort(array: MonitorowanaTablica):
+    left = 0
+    right = len(array) - 1
+
+    h = 1
+    while h <= (right - left) // 9:
+        h = 3 * h + 1
+
+    while h > 0:
+        for i in range(left + h, right + 1):
+            j = i
+
+            item = array[i]
+            while j >= left + h and item < array[j - h]:
+                array[j] = array[j - h]
+                j = j - h
+            array[j] = item
+
+        h = h // 3
+
+
+
+
+def merge_sort(array: MonitorowanaTablica, left=None, right=None):
+    if left is None:
+        left = 0
+    if right is None:
+        right = len(array) - 1
+
+    if left < right:
+        mid = (left + right) // 2
+        merge_sort(array, left, mid)
+        merge_sort(array, mid + 1, right)
+        merge(array, left, mid, right)
+
+def merge(array: MonitorowanaTablica, left, mid, right):
+    temp = [0] * (right - left + 1)
+    i, j, k = left, mid + 1, 0
+
+    while i <= mid and j <= right:
+        if array[i] <= array[j]:
+            temp[k] = array[i]
+            i += 1
+        else:
+            temp[k] = array[j]
+            j += 1
+        k += 1
+
+    while i <= mid:
+        temp[k] = array[i]
+        i += 1
+        k += 1
+
+
+    while j <= right:
+        temp[k] = array[j]
+        j += 1
+        k += 1
+
+    for k in range(len(temp)):
+        array[left + k] = temp[k]
+
+
+def quick_sort(array: MonitorowanaTablica, left=None, right=None):
+    if left is None:
+        left = 0
+    if right is None:
+        right = len(array) - 1
+
+    if left < right:
+        pivot_index = partition(array, left, right)
+        quick_sort(array, left, pivot_index - 1)
+        quick_sort(array, pivot_index + 1, right)
+
+def partition(array: MonitorowanaTablica, left, right):
+    pivot = array[right]
+    i = left - 1
+
+    for j in range(left, right):
+        if array[j] <= pivot:
+            i += 1
+            array[i], array[j] = array[j], array[i]
+
+    array[i + 1], array[right] = array[right], array[i + 1]
+    return i + 1
+
+
+def tim_sort(array: MonitorowanaTablica):
+    min_run = 32
+    n = len(array)
+
+
+    for start in range(0, n, min_run):
+        end = min(start + min_run - 1, n - 1)
+        insertion_sort(array, start, end)
+
+    size = min_run
+    while size < n:
+        for left in range(0, n, 2 * size):
+            mid = min(n - 1, left + size - 1)
+            right = min((left + 2 * size - 1), (n - 1))
+            if mid < right:
+                merge(array, left, mid, right)
+        size *= 2
+
+
+algorytmy = [
+    (insertion_sort, "Insertion Sort"),
+    (bubble_sort, "Bubble Sort"),
+    (shell_sort, "Shell Sort"),
+    (merge_sort, "Merge Sort"),
+    (quick_sort, "Quick Sort"),
+    (tim_sort, "Tim Sort"),
+]
---
+++ zestaw-5/ZADANIE2/maximize_it.py
@@ -0,0 +1,21 @@
+from itertools import product
+
+
+
+def maximize_expression(K, M, lists):
+    max_value=0
+    for combination in product(*lists):
+        current_sum = sum(x ** 2 for x in combination)
+        current_value = current_sum % M
+        max_value = max(max_value, current_value)
+    return max_value
+
+
+
+if __name__ == "__main__":
+    K, M = map(int, input().rstrip().split())
+
+    lists = [list(map(int, input().rstrip().split()[1:])) for _ in range(K)]
+
+    result = maximize_expression(K, M, lists)
+    print(result)
